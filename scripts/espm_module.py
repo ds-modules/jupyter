db38827489d2162aea7310bc78cc2f5bd4ba5808
Christopher Hench <chris.l.hench@gmail.com>
2017-10-05 12:34:14 -0700

    chris mapping edits

--- scripts/espm_module.py
+++ scripts/espm_module.py
@@ -2,10 +2,53 @@ import requests
 import os
 from shapely import geometry as sg, wkt
 import pandas as pd
+import random
 
 
 def to_fahren(val):
     return (val - 273.15) * 9 / 5 + 32
+
+
+def gen_hex_colour_code():
+    return ''.join([random.choice('0123456789ABCDEF')
+                    for x in range(6)])  # Make a random string with the letters
+    # in the string '0123456789ABCDEF'.
+
+
+def assign_colors(records_df, col_name):
+    color_dict = {}
+    for i, c in enumerate(list(set(records_df[col_name]))):
+        # We map an institute to a different random color.
+        color_dict[c] = "#" + gen_hex_colour_code()
+    color_dict
+
+    base_html = '''<tbody><tr>
+    <th style="width:25%">Collection</th>
+    <th style="width:15%">HEX</th>
+    <th style="width:43%">Color</th>
+    </tr>
+    '''
+
+    add_template = '''
+    <tr>
+    <td>COLLECTION&nbsp;</td>
+    <td>HEX</td>
+    <td style="background-color:HEX">&nbsp;</td>
+    </tr>
+    </tbody>
+    '''
+
+    to_render = base_html
+
+    for k in color_dict.keys():
+        to_render += add_template.replace(
+            'COLLECTION', k).replace(
+            'HEX', color_dict[k])
+
+    to_render += '</tbody>'
+    to_render = to_render.replace('Collection', col_name)
+
+    return (color_dict, to_render)
 
 
 class CalAdaptRequest(object):
